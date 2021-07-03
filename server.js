aa69b3b5b19a3c658a5bc3ecc445f82be72d395b
Kukuh Ardia <ardiajuniar@gmail.com>
2021-07-03 16:46:15 +0700

    Mongo DB, mongoose setup

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectToDB = require('./config/db');
 // const logger = require('./middleware/logger'); // Morgan but handmade
-
-// Import route files
-const bootcamps = require('./routes/bootcamp');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectToDB();
+
+// Import route files
+const bootcamps = require('./routes/bootcamp');
 
 const app = express();
 
@@ -26,7 +30,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode port on ${PORT}`)
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit
+  server.close(() => process.exit(1));
+})
