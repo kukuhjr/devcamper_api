3983f98b969ae6e7d129f42cc4872a0aab2b2dcd
kukuhjr <kukuhardia@gmail.com>
2021-07-17 12:49:53 +0700

    Create seeder for bootcamps data

--- server.js
+++ server.js
@@ -33,14 +33,14 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode port on ${PORT}`.yellow.bold)
-  );
+);
   
-  // Handle unhandled promise rejection
-  process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
-    // Close server & exit
-    server.close(() => process.exit(1));
-  })
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit
+  server.close(() => process.exit(1));
+})
   
 
 
