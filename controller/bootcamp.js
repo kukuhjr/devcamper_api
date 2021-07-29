af13984613963f6bd4683be17d03ca1aff836a2d
kukuhjr <kukuhardia@gmail.com>
2021-07-29 08:02:15 +0700

    Adding pagination

--- controller/bootcamp.js
+++ controller/bootcamp.js
@@ -8,18 +8,16 @@ const geocoder = require('../utils/geoCoder')
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
     let query;
-    // Copy req query
+    // Copy from req.query
     const reqQuery = { ...req.query }
     // Fields to exclude
-    const removeFields = ['select', 'sort']
-    // console.log(`Remove fields = ${removeFields}`);
+    const removeFields = ['select','sort','page','limit']
     // loop over removeFields and delete them from reqQuery
     removeFields.forEach(el => delete reqQuery[el]);
     // Create query String
     let queryStr = JSON.stringify(reqQuery)
     // Create Operators ($gt, $lt, $in)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
-    console.log(`Query String= ${queryStr}`);
     
     // Fetch Bootcamp data with query
     query = Bootcamp.find(JSON.parse(queryStr));
@@ -28,19 +26,46 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     if (req.query.select) {
         const fields = req.query.select.split(',').join(' ')
         query.select(fields);
-        console.log(fields);
+        console.log(`select: ${fields}`);
     }
     // Sort Fields
     if (req.query.sort) {
         const sortBy = req.query.sort.split(',').join(' ')
         query.sort(sortBy);
-        console.log(sortBy);
+        console.log(`sort: ${sortBy}`);
     }else{
         query.sort('-createdAt');
     }
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 25
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Bootcamp.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
+
+    // Execute Query
     const bootcamps = await query;
+
+    // Pagination
+    const pagination = {}
+    // next pagination
+    if (endIndex < total) {
+        pagination.next = {
+            next: page + 1,
+            limit
+        }
+    }
+    // prev pagination
+    if (startIndex > 0) {
+        pagination.prev = {
+            prev: page - 1,
+            limit
+        }
+    }
     
-    res.status(200).json({ success: true, counts: bootcamps.length ,data: bootcamps })
+    res.status(200).json({ success: true, counts: bootcamps.length, pagination, data: bootcamps })
 })
 
 // @desc    Get bootcamp by id
